b0ea6f9d1dc190e02df6ea983f56286a94acd1dd
Ha Can <canngocha2k1@gmail.com>
2024-01-31 07:26:52 +0700

    Update 4

--- src/assets/images/image.js
+++ src/assets/images/image.js
@@ -5,8 +5,9 @@ const images = {
     icon_home: require('./icon_home.svg').default,
     icon_lich: require('./icon_lich.svg').default,
     icon_question: require('./question.svg').default,
-    icon_setting: require('./setting.svg').default,
-    icon_add: require('./icon_add.svg').default,
+    icon_setting: require('./setting.svg').default, 
+    icon_add: require('./icon_add.svg').default, 
+    icon_GeoBay: require('./geoBay.svg').default,
 
     testImage: require('./testThongKe.svg').default,
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,7 +21,7 @@ function Header() {
             <Navbar.Collapse id="basic-navbar-nav me-auto">
               <Nav className={cx("navbar_nav")} style={{color:'#333'}}>
                 <Nav.Link as={NavLink} to="/deploygiaodien" className={`${cx("ht_geo")}` } >
-                <img src={images.logo} alt="Ht-geo" />
+                <img src={images.icon_GeoBay} alt="Ht-geo" />
                 </Nav.Link>
                 <Nav.Link as={NavLink} to="/upload" className={`${cx("nav_item")} ` }>Công việc</Nav.Link>
                 <Nav.Link as={NavLink} to="/main" className={`${cx("nav_item")} ` }>Dữ liệu</Nav.Link>
